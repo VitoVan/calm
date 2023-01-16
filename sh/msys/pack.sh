ac0485c7b67855516c4aa816a8b653b54180ce62
Vito Van <vito@sdf.org>
2023-01-16 09:48:28 +0800

    refactor: change release file name
    replace `uname` with Linux / macOS / Windows

--- sh/msys/pack.sh
+++ sh/msys/pack.sh
@@ -1,13 +1,13 @@
-zip -r -9 ./calm-$(uname | cut -d '_' -f 1).zip build.sh calm calm.exe calm.c build.bat entry.lisp src lib sh quicklisp README.md calm.asd sbcl images LICENSE
+zip -r -9 ./calm-Windows.zip build.sh calm calm.exe calm.c build.bat entry.lisp src lib sh quicklisp README.md calm.asd sbcl images LICENSE
 
 cd .github/workflows/
 
 ../../calm dist
 chmod +x ./dist/calm
 mv ./dist ./calm-app
-zip -r -9 ../../calm-app-$(uname | cut -d '_' -f 1).zip ./calm-app
+zip -r -9 ../../calm-app-Windows.zip ./calm-app
 
 ../../calm dist-with-canvas
 chmod +x ./dist-with-canvas/calm
 mv ./dist-with-canvas ./calm-app-with-canvas
-zip -r -9 ../../calm-app-with-canvas-$(uname | cut -d '_' -f 1).zip ./calm-app-with-canvas
+zip -r -9 ../../calm-app-with-canvas-Windows.zip ./calm-app-with-canvas
