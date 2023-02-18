30e3b772e7d6c56d9f5685254826941557abe3f7
Vito Van <vito@sdf.org>
2023-02-18 08:12:26 +0800

    bump version and fix bundleID for hello app

--- sh/darwin/pack.sh
+++ sh/darwin/pack.sh
@@ -7,12 +7,12 @@ else
 fi
 
 export APP_ICON=./build/calm.icns
-export APP_VERSION=0.0.29
-export APP_ID=com.vitovan.calm
+export APP_VERSION=0.0.30
 
 mkdir calm-dist
 cp -R {calm,calm.c,build,entry.lisp,src,lib,sh,quicklisp,README.md,calm.asd,sbcl,images,LICENSE} ./calm-dist/
 export APP_NAME=Calm
+export APP_ID=com.vitovan.calm
 export DIST_DIR=./calm-dist
 ./calm sh darwin bundle
 rm Calm.app/Contents/MacOS/.please_load_calm_canvas_from_here
@@ -27,6 +27,7 @@ cd .github/workflows/
 ../../calm dist
 cd ../../
 export APP_NAME=Hello
+export APP_ID=com.vitovan.hellocalm
 export DIST_DIR=./.github/workflows/dist
 ./calm sh darwin bundle
 export DMG_NAME="hello${DMG_SUFIX}$"
@@ -36,6 +37,7 @@ cd .github/workflows/
 ../../calm dist-with-canvas
 cd ../../
 export APP_NAME="Hello Canvas"
+export APP_ID=com.vitovan.hellocanvas
 export DIST_DIR=./.github/workflows/dist-with-canvas
 ./calm sh darwin bundle
 export DMG_NAME="hello-canvas${DMG_SUFIX}"
