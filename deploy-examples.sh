720f8299f6f59ac64709d976e367faefc8bd0a0f
Vito Van <vito@sdf.org>
2023-05-05 16:56:52 +0800

    Deploy examples to gh-pages

--- deploy-examples.sh
+++ deploy-examples.sh
@@ -1,26 +1,35 @@
-# Delete the ref to avoid keeping history.
-git update-ref -d refs/heads/gh-pages
-rm -rf ./*/
+#!/bin/bash
 
-curl -LO https://github.com/VitoVan/calm/releases/download/examples-0.0.42/Circles-web.zip
+if [[ ! -v CALM_EXAMPLE_VERSION ]]; then
+    echo "CALM_EXAMPLE_VERSION is not set"
+    exit 42
+fi
+
+rm -rf ./*/${CALM_EXAMPLE_VERSION}
+
+mkdir ${CALM_EXAMPLE_VERSION}
+cd ${CALM_EXAMPLE_VERSION}
+
+curl -LO https://github.com/VitoVan/calm/releases/download/${CALM_EXAMPLE_VERSION}/Circles-web.zip
 unzip Circles-web.zip
 mv web circles
 
-curl -LO https://github.com/VitoVan/calm/releases/download/examples-0.0.42/Fan-web.zip
+curl -LO https://github.com/VitoVan/calm/releases/download/${CALM_EXAMPLE_VERSION}/Fan-web.zip
 unzip Fan-web.zip
 mv web fan
 
-curl -LO https://github.com/VitoVan/calm/releases/download/examples-0.0.42/Meditator-web.zip
+curl -LO https://github.com/VitoVan/calm/releases/download/${CALM_EXAMPLE_VERSION}/Meditator-web.zip
 unzip Meditator-web.zip
 mv web meditator
 
-curl -LO https://github.com/VitoVan/calm/releases/download/examples-0.0.42/Mondrian-web.zip
+curl -LO https://github.com/VitoVan/calm/releases/download/${CALM_EXAMPLE_VERSION}/Mondrian-web.zip
 unzip Mondrian-web.zip
 mv web mondrian
 
 rm *.zip
 
+cd ..
+
 git add .
 git commit -m "Deploy examples to gh-pages"
-git push --force --set-upstream origin gh-pages
-
+git push --set-upstream origin gh-pages
