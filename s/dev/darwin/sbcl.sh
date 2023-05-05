9414dec2bd38ab477bd10d96b52a741a000f2877
Vito Van <vito@sdf.org>
2023-05-05 14:08:21 +0800

    upgrade sbcl dependency

--- s/dev/darwin/sbcl.sh
+++ s/dev/darwin/sbcl.sh
@@ -3,9 +3,9 @@ if ! [ -d sbcl ]; then
     if ! [ -f install_root.zip ]; then
         set -x
         if [[ -z "${CI_MATRIX_OS}" ]]; then
-            curl -o install_root.zip -L https://github.com/VitoVan/calm/releases/download/sbcl-2.3.2/install_root-macos-12.zip
+            curl -o install_root.zip -L https://github.com/VitoVan/calm/releases/download/sbcl-2.3.4/install_root-macos-13.zip
         else
-            curl -o install_root.zip -L https://github.com/VitoVan/calm/releases/download/sbcl-2.3.2/install_root-${CI_MATRIX_OS}.zip
+            curl -o install_root.zip -L https://github.com/VitoVan/calm/releases/download/sbcl-2.3.4/install_root-${CI_MATRIX_OS}.zip
         fi
         set +x
     fi
