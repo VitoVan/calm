VitoVan/calm

commit e76cee4639fe164a69c47b1e1923a34a3dc7f9b0

fix: no audio device while running CI
/fix https://github.com/VitoVan/calm/issues/127

what if they don't have the audio device?

src/c.lisp
@@ -128,7 +128,10 @@
 
 (defun open-audio-if-not-yet ()
   ;; (format t "open-audio-if-not-yet, already opened?: ~A~%" calm::*calm-state-audio-open*)
-  (unless calm::*calm-state-audio-open*
+  (when (and
+         (not calm::*calm-state-audio-open*) ;; not opened yet
+         (null (uiop:getenv "CI")) ;; not CI
+         )
     ;;
     ;; if we put the following code outside of this function,
     ;; it will open-audio right after the library is loaded,
@@ -158,66 +161,66 @@
 
 (defun play-music (pathname &key (loops 0))
   ;; (format t "playing music: ~A~%" pathname)
-  (open-audio-if-not-yet)
-  (let* ((music-pathname
-           #+jscl
-           (concatenate 'string "/usr/share/" pathname) ;; /usr/share/assets/ will be embedded
-           #-jscl
-           (or (uiop:absolute-pathname-p pathname)
-               (uiop:merge-pathnames* pathname (uiop:getenv "CALM_APP_DIR"))))
-         (music-object-cache
-           (cdr (assoc music-pathname calm::*calm-state-loaded-audio*
-                       #+jscl :test #+jscl #'string=)))
-         (music-object (or music-object-cache
-                           #+jscl
-                           (let ((music-pathname-ptr (#j:allocateUTF8 music-pathname)))
-                             (prog1 (#j:_Mix_LoadMUS music-pathname-ptr)
-                               (#j:_free music-pathname-ptr)))
-                           #-jscl
-                           (sdl2-mixer:load-music music-pathname))))
-    (unless music-object-cache
-      (push (cons music-pathname music-object) calm::*calm-state-loaded-audio*))
-    #+jscl
-    (#j:_Mix_PlayMusic music-object loops)
-    #-jscl
-    (sdl2-mixer:play-music music-object loops)))
+  (when (open-audio-if-not-yet)
+    (let* ((music-pathname
+             #+jscl
+             (concatenate 'string "/usr/share/" pathname) ;; /usr/share/assets/ will be embedded
+             #-jscl
+             (or (uiop:absolute-pathname-p pathname)
+                 (uiop:merge-pathnames* pathname (uiop:getenv "CALM_APP_DIR"))))
+           (music-object-cache
+             (cdr (assoc music-pathname calm::*calm-state-loaded-audio*
+                         #+jscl :test #+jscl #'string=)))
+           (music-object (or music-object-cache
+                             #+jscl
+                             (let ((music-pathname-ptr (#j:allocateUTF8 music-pathname)))
+                               (prog1 (#j:_Mix_LoadMUS music-pathname-ptr)
+                                 (#j:_free music-pathname-ptr)))
+                             #-jscl
+                             (sdl2-mixer:load-music music-pathname))))
+      (unless music-object-cache
+        (push (cons music-pathname music-object) calm::*calm-state-loaded-audio*))
+      #+jscl
+      (#j:_Mix_PlayMusic music-object loops)
+      #-jscl
+      (sdl2-mixer:play-music music-object loops))))
 
 (defun play-wav (pathname &key (loops 0) (channel -1))
   ;; (format t "playing wav: ~A~%" pathname)
-  (open-audio-if-not-yet)
-  (let* ((wav-pathname
-          #+jscl
-           (concatenate 'string "/usr/share/" pathname) ;; /usr/share/assets/ will be embedded
-           #-jscl
-           (or (uiop:absolute-pathname-p pathname)
-               (uiop:merge-pathnames* pathname (uiop:getenv "CALM_APP_DIR"))))
-         (wav-object-cache
-          (cdr (assoc wav-pathname calm::*calm-state-loaded-audio*
-                      #+jscl :test #+jscl #'string=)))
-         (wav-object (or wav-object-cache
-                         #+jscl
-                         (let* ((wav-pathname-allocated-ptr (#j:allocateUTF8 wav-pathname))
-                                (rb-allocated-ptr (#j:allocateUTF8 "rb"))
-                                (rwops (#j:_SDL_RWFromFile wav-pathname-allocated-ptr rb-allocated-ptr))
-                                (spec (#j:_Mix_LoadWAV_RW rwops 1 nil nil nil)))
-                           (#j:_free wav-pathname-allocated-ptr)
-                           (#j:_free rb-allocated-ptr)
-                           spec)
-                         #-jscl
-                         (sdl2-mixer:load-wav wav-pathname))))
-    (unless wav-object-cache
-      (push (cons wav-pathname wav-object) calm::*calm-state-loaded-audio*))
+  (when (open-audio-if-not-yet)
+    (let* ((wav-pathname
+             #+jscl
+             (concatenate 'string "/usr/share/" pathname) ;; /usr/share/assets/ will be embedded
+             #-jscl
+             (or (uiop:absolute-pathname-p pathname)
+                 (uiop:merge-pathnames* pathname (uiop:getenv "CALM_APP_DIR"))))
+           (wav-object-cache
+             (cdr (assoc wav-pathname calm::*calm-state-loaded-audio*
+                         #+jscl :test #+jscl #'string=)))
+           (wav-object (or wav-object-cache
+                           #+jscl
+                           (let* ((wav-pathname-allocated-ptr (#j:allocateUTF8 wav-pathname))
+                                  (rb-allocated-ptr (#j:allocateUTF8 "rb"))
+                                  (rwops (#j:_SDL_RWFromFile wav-pathname-allocated-ptr rb-allocated-ptr))
+                                  (spec (#j:_Mix_LoadWAV_RW rwops 1 nil nil nil)))
+                             (#j:_free wav-pathname-allocated-ptr)
+                             (#j:_free rb-allocated-ptr)
+                             spec)
+                           #-jscl
+                           (sdl2-mixer:load-wav wav-pathname))))
+      (unless wav-object-cache
+        (push (cons wav-pathname wav-object) calm::*calm-state-loaded-audio*))
 
-    ;; only play when there is/are available channel(s)
-    (when (< (playing) calm::*calm-audio-numchans*)
-      #+jscl
-      (#j:_Mix_PlayChannelTimed channel wav-object loops -1)
-      #-jscl
-      (sdl2-mixer:play-channel channel wav-object loops))))
+      ;; only play when there is/are available channel(s)
+      (when (< (playing) calm::*calm-audio-numchans*)
+        #+jscl
+        (#j:_Mix_PlayChannelTimed channel wav-object loops -1)
+        #-jscl
+        (sdl2-mixer:play-channel channel wav-object loops)))))
 
 #+jscl
 (defun play-audio (audio-url &key (loop-audio-p nil) (volume 1))
-  (format t "playing audio: ~A~%" audio-url)
+  ;; (format t "playing audio: ~A~%" audio-url)
   (let* ((audio-object-cache
            (cdr (assoc audio-url calm::*calm-state-loaded-audio* :test #'string=)))
          (audio-object

src/events.lisp
@@ -1,28 +1,23 @@
 (in-package :calm)
 
 (defun internal-on-textinput (text)
-  (c:open-audio-if-not-yet)
   (when (fboundp 'on-textinput)
     (on-textinput text)))
 
 (defun internal-on-keydown (key)
-  (c:open-audio-if-not-yet)
   (setf *calm-redraw* t)
   (when (fboundp 'on-keydown)
     (on-keydown key)))
 (defun internal-on-keyup (key)
-  (c:open-audio-if-not-yet)
   (setf *calm-redraw* t)
   (when (fboundp 'on-keyup)
     (on-keyup key)))
 
 (defun internal-on-mousewheel (x y direction)
-  (c:open-audio-if-not-yet)
   (setf *calm-redraw* t)
   (when (fboundp 'on-mousewheel)
     (on-mousewheel x y direction)))
 (defun internal-on-mousemotion (&key x y)
-  (c:open-audio-if-not-yet)
   (setf *calm-state-mouse-x* x
         *calm-state-mouse-y* y
         *calm-redraw* t
@@ -30,7 +25,6 @@
   (when (fboundp 'on-mousemotion)
     (on-mousemotion :x x :y y)))
 (defun internal-on-mousebuttonup (&key button x y clicks)
-  (c:open-audio-if-not-yet)
   (setf
    *calm-state-mouse-up* button
    *calm-state-mouse-down* nil
@@ -39,7 +33,6 @@
   (when (fboundp 'on-mousebuttonup)
     (on-mousebuttonup :button button :x x :y y :clicks clicks)))
 (defun internal-on-mousebuttondown (&key button x y clicks)
-  (c:open-audio-if-not-yet)
   (setf
    *calm-state-mouse-up* nil
    *calm-state-mouse-down* button
@@ -49,7 +42,6 @@
     (on-mousebuttondown :button button :x x :y y :clicks clicks)))
 
 (defun internal-on-fingermotion (&key x  y dx dy pressure finger-id)
-  (c:open-audio-if-not-yet)
   (setf *calm-state-finger-x* x
         *calm-state-finger-y* y
         *calm-redraw* t
@@ -58,7 +50,6 @@
   (when (fboundp 'on-fingermotion)
     (on-fingermotion :x x :y y :dx dx :dy dy :pressure pressure :finger-id finger-id)))
 (defun internal-on-fingerup (&key x  y dx dy pressure finger-id)
-  (c:open-audio-if-not-yet)
   (setf
    *calm-state-finger-up* finger-id
    *calm-state-finger-down* nil
@@ -68,7 +59,6 @@
   (when (fboundp 'on-fingerup)
     (on-fingerup :x x :y y :dx dx :dy dy :pressure pressure :finger-id finger-id)))
 (defun internal-on-fingerdown (&key x  y dx dy pressure finger-id)
-  (c:open-audio-if-not-yet)
   (setf
    *calm-state-finger-up* nil
    *calm-state-finger-down* finger-id

commit 8470771ce62bcf85824ecd2dacf4a5603602c49d

fix web open-audio bug introduced by e76cee4

src/c.lisp
@@ -130,7 +130,7 @@
   ;; (format t "open-audio-if-not-yet, already opened?: ~A~%" calm::*calm-state-audio-open*)
   (when (and
          (not calm::*calm-state-audio-open*) ;; not opened yet
-         (null (uiop:getenv "CI")) ;; not CI
+         #-jscl (null (uiop:getenv "CI")) ;; not CI
          )
     ;;
     ;; if we put the following code outside of this function,