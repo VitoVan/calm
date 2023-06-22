e76cee4639fe164a69c47b1e1923a34a3dc7f9b0
Vito Van <root@vitovan.com>
2023-06-22 09:52:00 +0800

    fix: no audio device while running CI
    /fix https://github.com/VitoVan/calm/issues/127
    
    what if they don't have the audio device?

--- src/events.lisp
+++ src/events.lisp
@@ -1,28 +1,23 @@
 (in-package :calm)
 
 (defun internal-on-textinput (text)
-  (c:open-audio-if-not-yet)
   (when (fboundp 'on-textinput)
     (on-textinput text)))
 
 (defun internal-on-keydown (key)
-  (c:open-audio-if-not-yet)
   (setf *calm-redraw* t)
   (when (fboundp 'on-keydown)
     (on-keydown key)))
 (defun internal-on-keyup (key)
-  (c:open-audio-if-not-yet)
   (setf *calm-redraw* t)
   (when (fboundp 'on-keyup)
     (on-keyup key)))
 
 (defun internal-on-mousewheel (x y direction)
-  (c:open-audio-if-not-yet)
   (setf *calm-redraw* t)
   (when (fboundp 'on-mousewheel)
     (on-mousewheel x y direction)))
 (defun internal-on-mousemotion (&key x y)
-  (c:open-audio-if-not-yet)
   (setf *calm-state-mouse-x* x
         *calm-state-mouse-y* y
         *calm-redraw* t
@@ -30,7 +25,6 @@
   (when (fboundp 'on-mousemotion)
     (on-mousemotion :x x :y y)))
 (defun internal-on-mousebuttonup (&key button x y clicks)
-  (c:open-audio-if-not-yet)
   (setf
    *calm-state-mouse-up* button
    *calm-state-mouse-down* nil
@@ -39,7 +33,6 @@
   (when (fboundp 'on-mousebuttonup)
     (on-mousebuttonup :button button :x x :y y :clicks clicks)))
 (defun internal-on-mousebuttondown (&key button x y clicks)
-  (c:open-audio-if-not-yet)
   (setf
    *calm-state-mouse-up* nil
    *calm-state-mouse-down* button
@@ -49,7 +42,6 @@
     (on-mousebuttondown :button button :x x :y y :clicks clicks)))
 
 (defun internal-on-fingermotion (&key x  y dx dy pressure finger-id)
-  (c:open-audio-if-not-yet)
   (setf *calm-state-finger-x* x
         *calm-state-finger-y* y
         *calm-redraw* t
@@ -58,7 +50,6 @@
   (when (fboundp 'on-fingermotion)
     (on-fingermotion :x x :y y :dx dx :dy dy :pressure pressure :finger-id finger-id)))
 (defun internal-on-fingerup (&key x  y dx dy pressure finger-id)
-  (c:open-audio-if-not-yet)
   (setf
    *calm-state-finger-up* finger-id
    *calm-state-finger-down* nil
@@ -68,7 +59,6 @@
   (when (fboundp 'on-fingerup)
     (on-fingerup :x x :y y :dx dx :dy dy :pressure pressure :finger-id finger-id)))
 (defun internal-on-fingerdown (&key x  y dx dy pressure finger-id)
-  (c:open-audio-if-not-yet)
   (setf
    *calm-state-finger-up* nil
    *calm-state-finger-down* finger-id
