c9d09c826b38dbea388c4bb8c9fb4f069a9feeb2
Vito Van <root@vitovan.com>
2023-07-03 17:46:48 +0800

    add lem dependencies

--- s/dev/all/quicklisp.sh
+++ s/dev/all/quicklisp.sh
@@ -8,4 +8,9 @@ if ! [ -d quicklisp ]; then
     ./calm sbcl \
            --load quicklisp.lisp \
            --load ./s/dev/all/install-quicklisp.lisp
+    cd quicklisp/local-projects/
+    git clone --depth 1 --recursive https://github.com/lem-project/lem.git
+    cd lem && git reset --hard 2b9b464de73e24cff9d0c34cceb9635c2eaed7c6 && cd ..
+    git clone --depth 1 https://github.com/sharplispers/log4cl.git
+    cd log4cl && git reset --hard fe3da517147d023029782ced7cd989ba24f1e62d && cd ..
 fi
