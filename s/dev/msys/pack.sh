2fbb4f035b437f72f52ee0a6fa9cea620c554d04
Vito Van <root@vitovan.com>
2023-07-03 19:34:42 +0800

    fix Windows lem.exe building

--- s/dev/msys/pack.sh
+++ s/dev/msys/pack.sh
@@ -2,5 +2,5 @@
 export PATH=$(pwd):$PATH
 
 mkdir /tmp/leca
-cp -R {lem.exe,calm.exe,calmNoConsole.exe,entry.lisp,build,src,lib,s,quicklisp,README.md,calm.asd,sbcl,images,LICENSE} /tmp/leca
+cp -R {lem.exe,lemNoConsole.exe,calm.exe,calmNoConsole.exe,entry.lisp,build,src,lib,s,quicklisp,README.md,calm.asd,sbcl,images,LICENSE} /tmp/leca
 cd /tmp && zip -r -9 ./leca.zip ./leca && cd - && mv /tmp/leca.zip .
